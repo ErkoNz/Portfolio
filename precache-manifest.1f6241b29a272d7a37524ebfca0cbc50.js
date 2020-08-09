3a996cdbbee35abd584225b2a0808af3e7d4a10e
ErkoN Z <erik.zabransky@gmail.com>
2020-08-09 13:06:59 +0200

    Updates

---
+++ precache-manifest.1f6241b29a272d7a37524ebfca0cbc50.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5cb5e06e2ee0713b595a80e12c858c4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d360edba2e346a5e9bb5",
+    "url": "/portfolio/static/css/main.2789bca3.chunk.css"
+  },
+  {
+    "revision": "f4977019ebd0c63f75c9",
+    "url": "/portfolio/static/js/2.b9785044.chunk.js"
+  },
+  {
+    "revision": "7918f9b893310890f0a444ed75c67550",
+    "url": "/portfolio/static/js/2.b9785044.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d360edba2e346a5e9bb5",
+    "url": "/portfolio/static/js/main.0235f0ca.chunk.js"
+  },
+  {
+    "revision": "e63ac8d85df9dec8b092",
+    "url": "/portfolio/static/js/runtime-main.b97a0a97.js"
+  },
+  {
+    "revision": "904f4abdac337ba780fda1444977df22",
+    "url": "/portfolio/static/media/Csharp.904f4abd.svg"
+  },
+  {
+    "revision": "ee5fca57ccd6293bbb13896836e69220",
+    "url": "/portfolio/static/media/about.ee5fca57.jpg"
+  },
+  {
+    "revision": "ac39e1d1b927dc294274fec310a7fb21",
+    "url": "/portfolio/static/media/corelDraw.ac39e1d1.svg"
+  },
+  {
+    "revision": "f305ebedd7b975f13edaabe85f23e0e5",
+    "url": "/portfolio/static/media/cpp.f305ebed.svg"
+  },
+  {
+    "revision": "65ca64b246c9a1ad71f6f9051912f9b0",
+    "url": "/portfolio/static/media/githubsvg.65ca64b2.svg"
+  },
+  {
+    "revision": "b74cb054e6feae66e6918e0927f28611",
+    "url": "/portfolio/static/media/movingPicCovid.b74cb054.jpg"
+  },
+  {
+    "revision": "077ece0c949e7f18bc9f621f98132155",
+    "url": "/portfolio/static/media/movingPicEminent.077ece0c.jpg"
+  },
+  {
+    "revision": "01125857de2236c554cfacd02017e37c",
+    "url": "/portfolio/static/media/myPic.01125857.png"
+  },
+  {
+    "revision": "a25cf04f90c2302c9bcb7e8b5fab4c50",
+    "url": "/portfolio/static/media/restApi.a25cf04f.svg"
+  },
+  {
+    "revision": "e6e6c457a159862d55e63b148a074434",
+    "url": "/portfolio/static/media/sonyVegas.e6e6c457.svg"
+  },
+  {
+    "revision": "1e0ca6abd5d467f393778a99bd5a7ba1",
+    "url": "/portfolio/static/media/sqlIcon.1e0ca6ab.svg"
+  }
+]);
